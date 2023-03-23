8ddf724e305f73ff16976468ff673288115245a4
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-03-23 14:53:12 +0000

    fix bubble sort

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,8 +1,15 @@
-
-
 pub fn sort(mut arr: Vec<i32>) -> Vec<i32> {
-     
-     return arr;
+    let length = arr.len();
+    for i in 0..(length - 1) {
+        for j in 0..((length - 1) - i) {
+            if arr[j] > arr[j + 1] {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
 
 #[cfg(test)]
@@ -11,9 +18,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_format_empty_board() {
-        let mut array = vec![1, 4, 2, 3,1,5, 121331, 11];
+    fn test_bubble_sort() {
+        let array = vec![1, 4, 2, 3, 1, 5, 121331, 11];
         let expected = vec![1, 1, 2, 3, 4, 5, 11, 121331];
+        let subject = sort(array);
+        assert_eq!(expected, subject);
+    }
+
+    #[test]
+    fn test_small_array_bubble_sort() {
+        let array = vec![6, 4];
+        let expected = vec![4, 6];
         let subject = sort(array);
         assert_eq!(expected, subject);
     }
