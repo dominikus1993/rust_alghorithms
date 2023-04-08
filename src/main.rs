32e2269c723800d998936c7888149cfdbbb4eaa0
Dominik.Kotecki <dominikus1910@gmail.com>
2023-04-08 16:25:06 +0200

    add to lower and to upper case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod sorting;
 mod recursion;
+mod strings;
 mod data_structures;
 fn main() {
     for i in 1..10 {
---
+++ src/strings/case.rs
@@ -0,0 +1,55 @@
+
+pub fn upper_case(txt: &str) -> String {
+    let mut res = Vec::new();
+    res.reserve(txt.len());
+    for x in txt.as_bytes() {
+        let char_val = *x;
+        if char_val >= 97 && char_val <= 122 {
+            let new_char = char_val - 32;
+            res.push(new_char);
+        } else {
+            res.push(char_val);
+        }
+    }
+
+    let result = String::from_utf8(res).unwrap();
+    return result;
+}
+
+pub fn lower_case(txt: &str) -> String {
+    let mut res = Vec::new();
+    res.reserve(txt.len());
+    for x in txt.as_bytes() {
+        let char_val = *x;
+        if char_val >= 65 && char_val <= 90 {
+            let new_char = char_val + 32;
+            res.push(new_char);
+        } else {
+            res.push(char_val);
+        }
+    }
+
+    let result = String::from_utf8(res).unwrap();
+    return result;
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_upper_case() {
+        let array = "tEst";
+        let subject = upper_case(array);
+        assert_eq!("TEST", subject);
+    }
+
+    #[test]
+    fn test_lower_case() {
+        let array = "tEsT";
+        let subject = lower_case(array);
+        assert_eq!("test", subject);
+    }
+}
